b406c7b6bef81506b95c7e2d0e294f657f27401f
Akumanomi <akumanomi1988@gmail.com>
2023-08-10 21:03:16 +0200

    Add launchet

---
+++ Launcher/CSVToolsTestLauncher/ExtractedObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVToolsTestLauncher
+{
+    internal class ExtractedObject
+    {
+        public ExtractedObject()
+        {
+        }
+
+        public ExtractedObject(string? columna1, string? columna2, string? columna3, string? columna4, string? columna5)
+        {
+            Columna1 = columna1;
+            Columna2 = columna2;
+            Columna3 = columna3;
+            Columna4 = columna4;
+            Columna5 = columna5;
+        }
+
+        public string? Columna1 { get; set; }
+        public string? Columna2 { get; set; }
+        public string? Columna3 { get; set; }
+        public string? Columna4 { get; set; }
+        public string? Columna5 { get; set; }
+        
+        public override string  ToString()
+        {
+            return $"{nameof(Columna1)}:'{Columna1}'" +
+                $"{nameof(Columna2)}:'{Columna2}'" +
+                $"{nameof(Columna3)}:'{Columna3}'" +
+                $"{nameof(Columna4)}:'{Columna4}'" +
+                $"{nameof(Columna5)}:'{Columna5}'" ;
+        }
+    }
+
+}
---
+++ Launcher/CSVToolsTestLauncher/Program.cs
@@ -0,0 +1,10 @@
+﻿using CSVTools.InfoExtract;
+using CSVTools.InfoImport;
+using CSVToolsTestLauncher;
+using System.Runtime.CompilerServices;
+
+InfoExtractorCSV<ExtractedObject> infoExtractor = new(@"C\:Pruebacsv.csv");
+InfoImport<ExtractedObject> infoImport = new();
+List<ExtractedObject> extractedObjects = infoExtractor.Extract();
+extractedObjects.ForEach(x => Console.WriteLine(x.ToString()));
+infoImport.Import(extractedObjects, @$"C:\Pruebacsv{DateTime.Now.Ticks}.csv");
